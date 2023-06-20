05d8e1a4862c5001d97021da5a73e334e265a173
ReshmaRajK <reshmarajmsm@gmail.com>
2023-06-20 21:16:49 +0530

    percentage function added to percentage button

--- calc.js
+++ calc.js
@@ -10,6 +10,9 @@ function displayOutput(){
 function zeroDisplay(){
     num.value+="00"
 }
+function displayModulus(){
+   num.value=(num.value/100)
+}
 function backSpace(){
 //current content got slice function,then this value after slice() it stored in input(i.e,num.value)
     num.value=num.value.slice(0,-1)
