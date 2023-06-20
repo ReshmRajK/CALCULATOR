7f66d3f1670c93e8f210cdf0daf475a1ed7c0351
ReshmaRajK <reshmarajmsm@gmail.com>
2023-06-20 11:26:46 +0530

    finally committed

--- calc.js
+++ calc.js
@@ -7,6 +7,9 @@ function allClear(){
 function displayOutput(){
     num.value=eval(num.value)
 }
+function zeroDisplay(){
+    num.value+="00"
+}
 function backSpace(){
 //current content got slice function,then this value after slice() it stored in input(i.e,num.value)
     num.value=num.value.slice(0,-1)
