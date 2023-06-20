e7cfc6ef37861c1547698460f8131d75a0600bef
ReshmaRajK <reshmarajmsm@gmail.com>
2023-06-20 11:07:13 +0530

    functionality added to calculator

---
+++ calc.js
@@ -0,0 +1,13 @@
+function displayData(data){
+    num.value=num.value+data
+}
+function allClear(){
+    num.value=""
+}
+function displayOutput(){
+    num.value=eval(num.value)
+}
+function backSpace(){
+//current content got slice function,then this value after slice() it stored in input(i.e,num.value)
+    num.value=num.value.slice(0,-1)
+}
